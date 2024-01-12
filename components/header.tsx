ada5ea995715336a88c7741f25e9db0550f622d3
julianastahelin <julianastahelin@gmail.com>
2024-01-12 17:20:20 -0300

    feat: adapt components' props and layout to fit pages

--- components/header.tsx
+++ components/header.tsx
@@ -1,30 +1,17 @@
-import { forwardRef, LegacyRef } from 'react'
 import Image from 'next/image'
+import Link from 'next/link'
 
-import { getSectionData, Header } from '@/lib/data/loader.ts'
+import { Header } from '@/lib/data/loader.ts'
 import profileImg from '@/public/assets/img/ju.jpg'
-import { MutableRef, SectionProps } from '@/types/core'
 
 
-interface HeaderProps extends SectionProps {
-    projectRef: MutableRef,
-}
-
-const Header = forwardRef(({ language, fadeIn, projectRef }: HeaderProps, ref: LegacyRef<HTMLElement>) => {
-
-    const header = getSectionData('Header', language) as Header
-
-    function scrollTo(ref: MutableRef) {
-        ref!.current!.scrollIntoView({ behavior: 'smooth' })
-    }
+export function HeaderSection({ data } : { data:Header }) {
 
     return (
-        <section 
-            ref={ref} 
-            className='
-                flex flex-col md:flex-row justify-evenly items-center 
-                px-7 md:px-16 min-[880px]:px-10 pb-12 pt-[70px] md:py-20 mt-[73px] scroll-mt-[72px]  
-                max-w-7xl
+        <section className='
+            flex flex-col md:flex-row justify-evenly items-center 
+            px-7 md:px-16 min-[880px]:px-10 pb-6 pt-[70px] md:py-20  
+            max-w-7xl      
             '
         >
             <div className='flex flex-col items-center gap-4 w-11/12 min-[500px]:w-2/3 md:w-2/5 text-center'>
@@ -35,10 +22,10 @@ const Header = forwardRef(({ language, fadeIn, projectRef }: HeaderProps, ref: L
                     priority
                 />
                 <h1 className='text-3xl sm:text-4xl'>
-                    {header.name}
+                    {data.name}
                 </h1>
-                <p className='font-light text:lg sm:text-xl' style={fadeIn}>
-                    {header.jobTitle}
+                <p className='font-light text:lg sm:text-xl'>
+                    {data.jobTitle}
                 </p>
             </div>
 
@@ -49,27 +36,23 @@ const Header = forwardRef(({ language, fadeIn, projectRef }: HeaderProps, ref: L
                 w-11/12 min-[500px]:w-2/3 md:w-1/2
                 bg-primary-foreground text-secondary-foreground border-4 border-border-tertiary shadow-medium-dark 
             '>
-                <p style={fadeIn}>
-                    {header.description}
+                <p>
+                    {data.description}
                 </p>
-                <button
+                <Link
                     className='
                         self-end p-2 mt-3
                         bg-inherit border-[1px] border-border-secondary shadow-big-light  
                         font-light text-sm text-secondary-foreground
                         hover:scale-105 hover:ease-in-out hover:opacity-70
                     '
-                    style={fadeIn}
-                    onClick={() => scrollTo(projectRef)}
+                    href='/projects'
                 >
                     &lt;<span className='text-accent-primary'>button</span>&gt;
-                    {header.projectsButton}
+                    {data.projectsButton}
                     &lt;<span className='text-accent-primary'>/button</span>&gt;
-                </button>
+                </Link>
             </div>
         </section>
     )
-})
-
-Header.displayName = 'Header'
-export default Header
+}
--- components/nav.tsx
+++ components/nav.tsx
@@ -1,32 +1,21 @@
-import { useState } from 'react'
+'use client'
+import { useContext, useState } from 'react'
+import Link from 'next/link'
 import ReactCountryFlag from 'react-country-flag'
 
 import { getSectionData, Nav } from '@/lib/data/loader.ts'
-import { Language, MutableRef, SectionProps } from '@/types/core'
+import { Language, LanguageContext } from '@/components/providers'
 
 
-interface NavProps extends SectionProps {
-    changeLang: (lang: Language) => void,
-    topRef: MutableRef,
-    techRef: MutableRef,
-    cvRef: MutableRef,
-    projectRef: MutableRef,
-    contactRef: MutableRef,
-}
-
-export default function Nav({ language, fadeIn, changeLang, topRef, techRef, cvRef, projectRef, contactRef }: NavProps) {
-
-    const nav = getSectionData('Nav', language) as Nav
+export function Nav() {
+    
+    const { language, setLanguage } = useContext(LanguageContext)
+    const data = getSectionData('Nav', language) as Nav
 
     const [open, setOpen] = useState<boolean>(false)
 
-    function scrollTo(ref: MutableRef) {
-        ref!.current!.scrollIntoView({ behavior: 'smooth' })
-        setOpen(!open)
-    }
-
-    function handleLanguageChange(lang: Language) {
-        changeLang(lang)
+    function handleLanguageChange(language: Language) {
+        setLanguage(language)
         setOpen(!open)
     }
 
@@ -34,10 +23,11 @@ export default function Nav({ language, fadeIn, changeLang, topRef, techRef, cvR
         <nav className='
             flex items-center justify-center 
             fixed top-0 z-10
-            min-h-[70px] w-full py-7 pl-14 pr-20 
+            min-h-[48px] w-full py-5 pl-14 pr-20 
             list-none
             bg-primary shadow-small-dark 
-        '>
+            '
+        >
             <ul className={`
                 flex flex-col md:flex-row flex-nowrap items-center justify-start md:justify-between gap-7 md:gap-14 lg:gap-32
                 h-full w-52 md:w-full max-w-[1440px] 
@@ -46,47 +36,27 @@ export default function Nav({ language, fadeIn, changeLang, topRef, techRef, cvR
                 bg-tertiary md:bg-inherit
                 transition-transform duration-300 ease-in-out ${open ? 'translate-x-0' : 'translate-x-full'} md:translate-x-0  
             `}>
-                <li
-                    className='text-tertiary-foreground md:text-primary-foreground hover:cursor-pointer'
-                    style={fadeIn}
-                    onClick={() => scrollTo(topRef)}
-                >
-                    {nav.home}
+                <li className='text-tertiary-foreground md:text-primary-foreground hover:cursor-pointer text-center'>
+                    <Link href='/' onClick={() => setOpen(!open)}>{data.home}</Link>
                 </li>
-                <li
-                    className='text-tertiary-foreground md:text-primary-foreground hover:cursor-pointer'
-                    style={fadeIn}
-                    onClick={() => scrollTo(techRef)}
-                >
-                    {nav.tech}
+                <li className='text-tertiary-foreground md:text-primary-foreground hover:cursor-pointer text-center'>
+                    <Link href='/tech-stack' onClick={() => setOpen(!open)}>{data.tech}</Link>
                 </li>
-                <li
-                    className='text-tertiary-foreground md:text-primary-foreground hover:cursor-pointer'
-                    style={fadeIn}
-                    onClick={() => scrollTo(cvRef)}
-                >
-                    {nav.cv}
+                <li className='text-tertiary-foreground md:text-primary-foreground hover:cursor-pointer text-center'>
+                    <Link href='/projects' onClick={() => setOpen(!open)}>{data.projects}</Link>
                 </li>
-                <li
-                    className='text-tertiary-foreground md:text-primary-foreground hover:cursor-pointer'
-                    style={fadeIn}
-                    onClick={() => scrollTo(projectRef)}
-                >
-                    {nav.projects}
+                <li className='text-tertiary-foreground md:text-primary-foreground hover:cursor-pointer text-center'>
+                    <Link href='/curriculum' onClick={() => setOpen(!open)}>{data.cv}</Link>
                 </li>
-                <li
-                    className='text-tertiary-foreground md:text-primary-foreground hover:cursor-pointer'
-                    style={fadeIn}
-                    onClick={() => scrollTo(contactRef)}
-                >
-                    {nav.contact}
+                <li className='text-tertiary-foreground md:text-primary-foreground hover:cursor-pointer text-center'>
+                    <Link href='/contact' onClick={() => setOpen(!open)}>{data.contact}</Link>
                 </li>
                 <div className='flex gap-4 md:gap-2'>
                     {/* TODO: map on languages array to define buttons dinamically */}
                     <button className='flex text-2xl hover:opacity-80' onClick={() => handleLanguageChange('pt')}>
                         <ReactCountryFlag countryCode='BR' svg style={{ borderRadius: 20 }} />
                     </button>
-                    <button className='flex text-2xl hover:opacity-80' onClick={() => handleLanguageChange('en')}>
+                    <button className='flex text-2xl hover:opacity-80' onClick={() =>  handleLanguageChange('en')}>
                         <ReactCountryFlag countryCode='GB' svg style={{ borderRadius: 20 }} />
                     </button>
                 </div>
@@ -96,13 +66,13 @@ export default function Nav({ language, fadeIn, changeLang, topRef, techRef, cvR
                 onClick={() => setOpen(!open)}
                 className='
                     flex md:hidden flex-col flex-nowrap justify-around
-                    w-8 h-8 fixed top-4 right-5 z-20 
+                    w-7 h-7 fixed top-2 right-5 z-20 
                     hover:opacity-80 hover:cursor-pointer
                 '
             >
-                <div className={`w-8 h-[2px] rounded-3xl transition-all duration-300 ease-linear origin-[1px] ${open ? 'bg-tertiary-foreground rotate-45' : 'bg-primary-foreground rotate-0'}`} />
-                <div className={`w-8 h-[2px] rounded-3xl transition-all duration-300 ease-linear origin-[1px] ${open ? 'bg-tertiary-foreground translate-x-full opacity-0' : 'bg-primary-foreground translate-x-0 opacity-100'}`} />
-                <div className={`w-8 h-[2px] rounded-3xl transition-all duration-300 ease-linear origin-[1px] ${open ? 'bg-tertiary-foreground rotate-[-45deg]' : 'bg-primary-foreground rotate-0'}`} />
+                <div className={`w-7 h-[2px] rounded-3xl transition-all duration-300 ease-linear origin-[1px] ${open ? 'bg-tertiary-foreground rotate-45' : 'bg-primary-foreground rotate-0'}`} />
+                <div className={`w-7 h-[2px] rounded-3xl transition-all duration-300 ease-linear origin-[1px] ${open ? 'bg-tertiary-foreground translate-x-full opacity-0' : 'bg-primary-foreground translate-x-0 opacity-100'}`} />
+                <div className={`w-7 h-[2px] rounded-3xl transition-all duration-300 ease-linear origin-[1px] ${open ? 'bg-tertiary-foreground rotate-[-45deg]' : 'bg-primary-foreground rotate-0'}`} />
             </div>
         </nav>
     )
