363b9b1e0d8fe6db60543b593cb51a20b57cddf5
julianastahelin <julianastahelin@gmail.com>
2024-01-12 17:18:16 -0300

    feat: create language context provider to manage state

---
+++ components/providers.tsx
@@ -0,0 +1,27 @@
+'use client'
+import { useState, createContext } from 'react'
+
+export type Language = 'pt' | 'en'
+
+interface LanguageContextType {
+    language: Language,
+    setLanguage: React.Dispatch<React.SetStateAction<Language>>,
+}
+
+const defaultLanguage = {
+    language: 'en' as Language,
+    setLanguage: () => {},
+}
+
+export const LanguageContext = createContext<LanguageContextType>(defaultLanguage)
+
+export function Providers({ children }: React.PropsWithChildren) {
+    
+    const [ language, setLanguage ] = useState<Language>(defaultLanguage.language)
+
+    return (
+        <LanguageContext.Provider value={{language, setLanguage}}>
+            {children}
+        </LanguageContext.Provider>
+    )
+}
