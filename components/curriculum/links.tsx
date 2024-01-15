0c956e6d5d29519a480ac2effac11998e887e42d
julianastahelin <julianastahelin@gmail.com>
2024-01-15 13:17:54 -0300

    feat: change curriculum text orientation from medium size and bigger

--- components/curriculum/links.tsx
+++ components/curriculum/links.tsx
@@ -10,16 +10,16 @@ interface LinksPartProps {
 export function LinksPart({ links }: LinksPartProps) {
 
     return (
-        <div className='flex flex-col md:flex-row items-center md:items-stretch w-full gap-2 md:gap-7'>
+        <div className='flex flex-col items-center w-full gap-2 md:gap-7'>
             <h3 className='
-                text-2xl text-center md:text-right
-                flex justify-center md:justify-end items-center 
-                w-1/2 md:w-1/3 pr-0 md:pr-4 pb-1
-                border-border-primary border-r-0 md:border-r-[1px] border-b-[1px] md:border-b-0
+                text-2xl text-center
+                flex justify-center items-center 
+                w-4/5 md:w-1/2 pr-0 md:pr-4 pb-1
+                border-border-primary border-r-0 border-b-[1px]
             '>
                 {links.title}
             </h3>
-            <div className='flex flex-col w-4/5 md:w-2/3 px-2 gap-5'>
+            <div className='flex flex-col w-4/5 md:w-1/2 px-2 gap-5'>
                 <ul className='list-none pl-2'>
                     {
                         links.list.map((link, index) =>
