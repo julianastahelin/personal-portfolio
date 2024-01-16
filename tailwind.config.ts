a5e9b9818985c93ef8eb1af419a8b159bc831504
julianastahelin <julianastahelin@gmail.com>
2024-01-15 23:14:13 -0300

    chore: init and config shadcn-ui

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -1,12 +1,22 @@
-import type { Config } from 'tailwindcss'
+import type { Config } from "tailwindcss"
 
-const config: Config = {
+const config = {
+  darkMode: ["class"],
   content: [
-    './pages/**/*.{js,ts,jsx,tsx,mdx}',
-    './components/**/*.{js,ts,jsx,tsx,mdx}',
-    './app/**/*.{js,ts,jsx,tsx,mdx}',
+    './pages/**/*.{ts,tsx}',
+    './components/**/*.{ts,tsx}',
+    './app/**/*.{ts,tsx}',
+    './src/**/*.{ts,tsx}',
   ],
+  prefix: "",
   theme: {
+    container: {
+      center: true,
+      padding: "2rem",
+      screens: {
+        "2xl": "1400px",
+      },
+    },
     extend: {
       fontFamily: {
         sans: ['var(--font-roboto-mono)'],
@@ -45,13 +55,23 @@ const config: Config = {
         'medium-light': '3px 3px 3px 0px rgba(255,255,255,0.5)',
         'big-light': '3px 3px 5px 0px rgba(255,255,255,0.8)',
       },
+      keyframes: {
+        "accordion-down": {
+          from: { height: "0" },
+          to: { height: "var(--radix-accordion-content-height)" },
+        },
+        "accordion-up": {
+          from: { height: "var(--radix-accordion-content-height)" },
+          to: { height: "0" },
+        },
+      },
+      animation: {
+        "accordion-down": "accordion-down 0.2s ease-out",
+        "accordion-up": "accordion-up 0.2s ease-out",
+      },
     },
   },
-  plugins: [],
-  variants: {
-    extend: {
-      textColor: ['visited'],
-    }
-  }
-}
+  plugins: [require("tailwindcss-animate")],
+} satisfies Config
+
 export default config
