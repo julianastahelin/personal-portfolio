4cc1f300983f490e5465144b18ccf72c335da512
julianastahelin <julianastahelin@gmail.com>
2024-01-22 10:29:28 -0300

    feat: define blog posts fetching and date converting functions

--- lib/utils.ts
+++ lib/utils.ts
@@ -1,6 +1,28 @@
-import { type ClassValue, clsx } from "clsx"
-import { twMerge } from "tailwind-merge"
+import { type ClassValue, clsx } from 'clsx'
+import { twMerge } from 'tailwind-merge'
+
+import { Language } from '@/components/providers'
+
 
 export function cn(...inputs: ClassValue[]) {
   return twMerge(clsx(inputs))
+}
+
+function isValidDate(date: Date) {
+  return !isNaN(+date)
+}
+
+export function convertDateFromIsoToDayMonthYear(isoDate: string, language: Language) {
+  var date = new Date(isoDate)
+  if (isValidDate(date)) {
+    return date.toLocaleDateString(
+      language,
+      {
+        year: 'numeric',
+        month: 'short',
+        day: '2-digit'
+      }
+    )
+  }
+  return null
 }
