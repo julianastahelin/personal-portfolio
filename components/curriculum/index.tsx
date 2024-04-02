89835649772448a1c1aec717e4541c63a17f92ea
julianastahelin <julianastahelin@gmail.com>
2024-04-02 15:02:17 -0300

    feat: update cv files and download button

--- components/curriculum/index.tsx
+++ components/curriculum/index.tsx
@@ -76,8 +76,8 @@ function DownloadPDFButton({ language }: { language: Language }) {
     }
 
     const pdfFileName = {
-        en: 'Juliana Coelho Stahelin - Front-end web developer',
-        pt: 'Juliana Coelho Stahelin - Desenvolvedora Front-end',
+        en: 'Juliana Coelho Stahelin - Full-stack web developer',
+        pt: 'Juliana Coelho Stahelin - Desenvolvedora Full-stack',
     }
 
     return (
