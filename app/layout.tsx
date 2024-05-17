3e2de5f30163e6e74a4fe058da30368dbf9fee1d
julianastahelin <julianastahelin@gmail.com>
2024-05-17 10:23:22 -0300

    feat: add initial metadata to main layout - in english

--- app/layout.tsx
+++ app/layout.tsx
@@ -4,8 +4,9 @@ import './globals.css'
 
 import { Providers } from '@/components/providers'
 import { CustomToaster } from '@/components/custom-ui/toaster'
-import { Nav } from '@/components/nav'
+import { NavBar } from '@/components/nav'
 import { Footer } from '@/components/footer'
+
 
 const roboto_mono = Roboto_Mono({
   subsets: ['latin'],
@@ -14,8 +15,32 @@ const roboto_mono = Roboto_Mono({
 })
 
 export const metadata: Metadata = {
-  title: 'Juliana Stahelin - Portfolio',
-  description: 'Frontend web developer - Next.js | Typescript | Tailwind | Git',
+  title: {
+    default: 'Juliana Stahelin - Front-end web developer | Portfolio',
+    template: '%s | Juliana Stahelin - Front-end web developer',
+  },
+  description: 'Driven front-end web developer with experience in React, Next.js, TypeScript, JavaScript, Tailwind, and Git. Eager learner dedicated to excellence.',
+  verification: {
+    google: 'zdhzXXAUX7whEn2K13bEyu5mGVEOjkozqtrtpsiVckA'
+  },
+  keywords: [ 'Front-end development', 'Front-end developer', 'Frontend developer', 'Software developer', 'React', 'Next.js', 'Typescript', 'Javascript', 'Tailwind', 'Git', 'Juliana Stahelin', 'Juliana Coelho Stahelin' ],
+  creator: 'Juliana Stahelin and Sabrina Stahelin',
+  publisher: 'Vercel',
+  openGraph: {
+    title: 'Juliana Stahelin - Front-end web developer | Portfolio',
+    description: 'Driven front-end web developer with experience in React, Next.js, TypeScript, JavaScript, Tailwind, and Git. Eager learner dedicated to excellence.',
+    type: 'website',
+    url: 'https://julianastahelin.vercel.app/',
+    siteName: 'Juliana Stahelin - Front-end web developer | Portfolio',
+    locale: 'en_US'
+  },
+  alternates: {
+    canonical: './',
+    languages: {
+      'pt-BR': '/',
+      'en-US': '/'
+    }
+  }
 }
 
 export default function RootLayout({
@@ -23,12 +48,11 @@ export default function RootLayout({
 }: {
   children: React.ReactNode
 }) {
-
   return (
     <html lang='en' className={`${roboto_mono.variable} font-sans`}>
       <body className='flex flex-col min-h-screen bg-primary'>
         <Providers>
-          <Nav />
+          <NavBar />
           <div className='mt-12 md:mt-16 py-10 flex justify-center flex-grow bg-primary break-words'>
             {children}
           </div>
